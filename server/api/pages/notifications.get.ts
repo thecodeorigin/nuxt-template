8f4e0bf0e2e0461702fcba7b1b15a42fa6820e35
Nguyễn Đức Truyền <118962054+NguyenDucTruyen@users.noreply.github.com>
2024-07-15 15:38:10 +0700

    Update server/api/pages/notifications.get.ts
    Co-authored-by: Rim (Y Nguyen) <46400321+imrim12@users.noreply.github.com>

--- server/api/pages/notifications.get.ts
+++ server/api/pages/notifications.get.ts
@@ -2,8 +2,6 @@ export default defineEventHandler(async (event) => {
   const { data } = await supabaseAdmin.from('sys_notifications').select()
   const { data: me } = await supabaseAdmin.from('sys_users').select().eq('email', 'admin@demo.com').maybeSingle()
   setResponseStatus(event, 200)
-  console.log('me:', me)
-  console.log('data:', data)
 
   return data
 })
