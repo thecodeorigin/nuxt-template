fba67d2fe6c50f62a8ff5e755a2033ef04e15bd7
TruyenBD <mrtruyenbd1407@gmail.com>
2024-07-17 22:56:54 +0700

    feat: infinite loading notifications

--- supabase/migrations/20240715182157_change_notification_id_type.sql
+++
@@ -1,8 +0,0 @@
-alter table "public"."sys_notifications" alter column "id" set default gen_random_uuid();
-
-alter table "public"."sys_notifications" alter column "id" drop identity;
-
-alter table "public"."sys_notifications" alter column "id" set data type uuid using "id"::uuid;
-
-
-
