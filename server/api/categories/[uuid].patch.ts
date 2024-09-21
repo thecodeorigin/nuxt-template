3ed6fec3d0f4c324fa0373a710c3253b5324c573
TruyenBD <mrtruyenbd1407@gmail.com>
2024-09-21 23:57:46 +0700

    feat: adapt to UI category page

--- server/api/categories/[uuid].patch.ts
+++ server/api/categories/[uuid].patch.ts
@@ -5,6 +5,13 @@ export default defineEventHandler(async (event) => {
     const { session, uuid } = await defineEventOptions(event, { auth: true, params: ['uuid'] })
     const user_id = session.user!.id!
     const body = await readBody(event)
+
+    if (body) {
+      if (body.created_at)
+        body.created_at = new Date(body.created_at)
+      if (body.updated_at)
+        body.updated_at = new Date(body.updated_at)
+    }
     const { updateCategoryById } = useCategoryCrud({ user_id })
     const response = await updateCategoryById(uuid, body)
 
