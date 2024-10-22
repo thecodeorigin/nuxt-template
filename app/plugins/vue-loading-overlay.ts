d0b0aa871a9b2d4765f12a97879cb3cfcb2f5ff7
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2024-10-23 02:24:54 +0700

    fix: plugin load mode

--- app/plugins/vue-loading-overlay.ts
+++
@@ -1,9 +0,0 @@
-import type { Props } from 'vue-loading-overlay'
-import { LoadingPlugin } from 'vue-loading-overlay'
-import 'vue-loading-overlay/dist/css/index.css'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const pluginOptions: Props = {}
-
-  nuxtApp.vueApp.use(LoadingPlugin, pluginOptions)
-})
