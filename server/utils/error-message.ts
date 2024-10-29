511766a3c6a1f84e1f596fab0a53d224122dbc5f
minhcu <minhdong1831999@gmail.com>
2024-10-29 23:26:06 +0700

    feat: add ErrorMessage EMAIL_ALREADY_VERIFIED

--- server/utils/error-message.ts
+++ server/utils/error-message.ts
@@ -9,4 +9,5 @@ export enum ErrorMessage {
   BAD_REQUEST = 'Bad request!',
   EMAIL_NOT_VERIFIED = 'Email not verified!',
   INVALID_VERIFICATION_URL = 'Invalid verification URL!',
+  EMAIL_ALREADY_VERIFIED = 'Email already verified!',
 }
