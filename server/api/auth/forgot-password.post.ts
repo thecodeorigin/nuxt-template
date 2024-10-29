645d201700b26a4227311bc7e11b0307bde2ea13
minhcu <minhdong1831999@gmail.com>
2024-10-30 00:03:39 +0700

    feat: reset password

---
+++ server/api/auth/forgot-password.post.ts
@@ -0,0 +1,46 @@
+import { createHmac } from 'node:crypto'
+import { Buffer } from 'node:buffer'
+import { eq } from 'drizzle-orm'
+import { sysUserTable } from '@base/server/db/schemas/sys_users.schema'
+
+export default defineEventHandler(async (event) => {
+  try {
+    const { email, type }: { email: string, type: string } = await readBody(event)
+
+    if (type !== 'reset' || !email) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: ErrorMessage.INVALID_VERIFICATION_URL,
+      })
+    }
+
+    const sysUser = await db.query.sysUserTable.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        email_verified: true,
+      },
+      where: eq(sysUserTable.email, email),
+    })
+
+    if (!sysUser) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: ErrorMessage.DONOT_HAVE_PERMISSION,
+      })
+    }
+
+    const runtimeConfig = useRuntimeConfig()
+    const { sendMail } = useNodeMailer()
+    const token = Buffer.from(`${email}^^${createHmac('sha256', runtimeConfig.auth.secret).update(`${email}${sysUser.id}`).digest('hex')}`).toString('base64')
+
+    await sendMail({
+      to: email,
+      subject: 'Password Reset',
+      text: `Please click on the link to reset your password: ${runtimeConfig.public.appBaseUrl}/auth/reset-password?token=${token}&type=reset&redirect_to=/auth/login`,
+    })
+  }
+  catch (error: any) {
+    throw parseError(error)
+  }
+})
