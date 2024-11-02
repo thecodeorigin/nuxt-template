f766c04f9fbccd32d0c96d5b250279cd8b88368c
TruyenBD <mrtruyenbd1407@gmail.com>
2024-11-03 01:01:37 +0700

    fix: redirect with full query

--- app/middleware/01.authentication.global.ts
+++ app/middleware/01.authentication.global.ts
@@ -9,7 +9,6 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const authStore = useAuthStore()
 
   const { signOut } = useAuth()
-
   if (authStore.isAuthenticated) {
     if (!authStore.currentUser?.id) {
       await signOut({ redirect: false })
@@ -19,11 +18,17 @@ export default defineNuxtRouteMiddleware(async (to) => {
     }
   }
   else if (!to.meta.unauthenticatedOnly && to.name !== 'auth-login') {
+    const filteredQuery = Object.entries(to.query)
+      .filter(([key]) => key !== 'to')
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&')
+
+    const fullPath = filteredQuery ? `${to.path}?${filteredQuery}` : to.path
+
     return navigateTo({
       name: 'auth-login',
       query: {
-        ...to.query,
-        to: to.fullPath !== '/' ? to.path : undefined,
+        to: to.fullPath !== '/' ? fullPath : undefined,
       },
     })
   }
