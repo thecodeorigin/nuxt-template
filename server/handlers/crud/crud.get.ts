ebb4dd319c10f32e7601511ff0bcb6fef72d1fba
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2024-11-10 14:31:12 +0700

    feat: crud handler

--- server/handlers/crud/crud.get.ts
+++ server/handlers/crud/crud.get.ts
@@ -1,8 +1,9 @@
 import { eq } from 'drizzle-orm'
 import type { PgTable } from 'drizzle-orm/pg-core'
 import type { EventHandlerRequest, H3Event } from 'h3'
+import type { HandlerOptions } from '.'
 
-export function _CrudGetHandler<T extends PgTable>(schema: T, event: H3Event<EventHandlerRequest>) {
+export function _CrudGetHandler<T extends PgTable>(schema: T, event: H3Event<EventHandlerRequest>, options?: HandlerOptions) {
   const id = getRouterParam(event, 'id')
 
   return db.select().from(schema).where(
--- server/handlers/crud/index.ts
+++ server/handlers/crud/index.ts
@@ -1,4 +1,5 @@
 import type { PgTable } from 'drizzle-orm/pg-core'
+import type { EventHandlerRequest, H3Event } from 'h3'
 
 import { match } from 'ts-pattern'
 
@@ -8,14 +9,35 @@ import { _CrudPutHandler } from './crud.put'
 import { _CrudPatchHandler } from './crud.patch'
 import { _CrudDeleteHandler } from './crud.delete'
 
-export function CrudHandler<T extends PgTable>(schema: T) {
+type TupleType<T extends string[]> = [...T]
+
+type Params<P extends string[]> = {
+  [K in TupleType<P>[number]]: string
+}
+
+export interface HandlerMethods<P extends string[]> {
+  get?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
+  post?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
+  put?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
+  patch?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
+  delete?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
+}
+
+export interface HandlerOptions<P extends string[]> {
+  params?: P
+  events?: HandlerMethods<P>
+  callbacks?: HandlerMethods<P>
+  overrides?: HandlerMethods<P>
+}
+
+export function CrudHandler<T extends PgTable, P extends string[]>(schema: T, options?: HandlerOptions<TupleType<P>>) {
   return eventHandler((event) => {
     return match(event)
-      .with({ method: 'GET' }, () => _CrudGetHandler(schema, event))
-      .with({ method: 'POST' }, () => _CrudPostHandler(schema, event))
-      .with({ method: 'PUT' }, () => _CrudPutHandler(schema, event))
-      .with({ method: 'PATCH' }, () => _CrudPatchHandler(schema, event))
-      .with({ method: 'DELETE' }, () => _CrudDeleteHandler(schema, event))
+      .with({ method: 'GET' }, () => _CrudGetHandler(schema, event, options))
+      .with({ method: 'POST' }, () => _CrudPostHandler(schema, event, options))
+      .with({ method: 'PUT' }, () => _CrudPutHandler(schema, event, options))
+      .with({ method: 'PATCH' }, () => _CrudPatchHandler(schema, event, options))
+      .with({ method: 'DELETE' }, () => _CrudDeleteHandler(schema, event, options))
       .otherwise(() => {
         throw createError({
           statusCode: 405,
