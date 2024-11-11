d7426ae87c6587926be03fed6240bedd9bcae8b8
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2024-11-12 01:12:53 +0700

    chore: remove error plugin to rely on sentry alert

--- server/handlers/crud/index.ts
+++ server/handlers/crud/index.ts
@@ -3,6 +3,7 @@ import type { EventHandlerRequest, H3Event } from 'h3'
 
 import { match } from 'ts-pattern'
 
+import type { Promisable } from 'type-fest'
 import { _CrudGetHandler } from './crud.get'
 import { _CrudPostHandler } from './crud.post'
 import { _CrudPutHandler } from './crud.put'
@@ -16,21 +17,20 @@ type Params<P extends string[]> = {
 }
 
 export interface HandlerMethods<P extends string[]> {
-  get?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
-  post?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
-  put?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
-  patch?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
-  delete?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => void
+  get?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => Promisable<void>
+  post?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => Promisable<void>
+  put?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => Promisable<void>
+  patch?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => Promisable<void>
+  delete?: (event: H3Event<EventHandlerRequest>, params: Params<P>) => Promisable<void>
 }
 
-export interface HandlerOptions<P extends string[]> {
+export interface HandlerOptions<P extends string[] = string[]> {
   params?: P
   events?: HandlerMethods<P>
-  callbacks?: HandlerMethods<P>
-  overrides?: HandlerMethods<P>
+  methods?: HandlerMethods<P>
 }
 
-export function CrudHandler<T extends PgTable, P extends string[]>(schema: T, options?: HandlerOptions<TupleType<P>>) {
+export function CrudHandler<T extends PgTable, P extends string[] = string[]>(schema: T, options?: HandlerOptions<TupleType<P>>) {
   return eventHandler((event) => {
     return match(event)
       .with({ method: 'GET' }, () => _CrudGetHandler(schema, event, options))
