ebfd5deb093fc4c5da614fe19358dfdf5f6bc961
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2024-11-12 01:12:53 +0700

    chore: remove error plugin to rely on sentry alert

--- server/handlers/crud/crud.get.ts
+++ server/handlers/crud/crud.get.ts
@@ -1,12 +1,29 @@
-import { eq } from 'drizzle-orm'
+import { and, eq } from 'drizzle-orm'
 import type { PgTable } from 'drizzle-orm/pg-core'
 import type { EventHandlerRequest, H3Event } from 'h3'
+import { camelCase, snakeCase } from 'lodash-es'
 import type { HandlerOptions } from '.'
 
-export function _CrudGetHandler<T extends PgTable>(schema: T, event: H3Event<EventHandlerRequest>, options?: HandlerOptions) {
-  const id = getRouterParam(event, 'id')
+export async function _CrudGetHandler<T extends PgTable>(schema: T, event: H3Event<EventHandlerRequest>, options?: HandlerOptions) {
+  const params = (options?.params || ['id'])?.map(param => ({
+    key: param,
+    value: getRouterParam(event, camelCase(param)) || '',
+  }))
 
-  return db.select().from(schema).where(
-    eq((schema as any).id, id),
-  )
+  let response: any
+
+  if (options?.methods?.get) {
+    response = await options?.methods?.get?.(event, params.map(p => p.value) as any)
+  }
+  else {
+    response = await db.select().from(schema).where(
+      and(
+        ...params.map(param => eq((schema as any)[snakeCase(param.key)], param.value)),
+      ),
+    )
+  }
+
+  options?.events?.get?.(event, params.map(p => p.value) as any)
+
+  return response
 }
