85941e2ed2c9a6f1a024ddf37e32fff4ed567c9d
TruyenBD <mrtruyenbd1407@gmail.com>
2024-11-12 22:42:56 +0700

    fix: detecting browser is opend inapp

--- app/utils/detectBrowser.ts
+++ app/utils/detectBrowser.ts
@@ -1,20 +1,17 @@
-export function isInBrowser() {
-  const userAgent = navigator.userAgent
-  const browsers = [
-    { name: 'Mozilla Firefox', keyword: 'Firefox' },
-    { name: 'Google Chrome', keyword: 'Chrome' },
-    { name: 'Apple Safari', keyword: 'Safari' },
-    { name: 'Opera', keyword: 'Opera' },
-    { name: 'Opera', keyword: 'OPR' },
-    { name: 'Microsoft Edge', keyword: 'Edge' },
-    { name: 'Internet Explorer', keyword: 'Trident' },
+export function isInAppBrowser() {
+  const { userAgent, isMobile } = useDevice()
+
+  const inAppKeywords = [
+    'FBAN',
+    'FBAV',
+    'Instagram',
+    'Messenger',
+    'Line',
+    'Snapchat',
+    'Twitter',
+    'WeChat',
+    'WhatsApp',
   ]
 
-  for (const browser of browsers) {
-    if (userAgent.includes(browser.keyword)) {
-      return browser.name
-    }
-  }
-
-  return false
+  return isMobile && inAppKeywords.some(keyword => userAgent.includes(keyword))
 }
