79309eef2d25a9a13fc6688a7efa1dbe4cea4cef
minhcu <minhdong1831999@gmail.com>
2024-11-21 19:59:04 +0700

    chore: vnpay always in test mode unless using production

--- server/utils/vnpay.ts
+++ server/utils/vnpay.ts
@@ -3,6 +3,6 @@ import { VNPay, ignoreLogger } from 'vnpay'
 export const vnpayAdmin = new VNPay({
   tmnCode: process.env.VNP_TMNCODE!,
   secureSecret: process.env.VNP_HASHSECRET!,
-  testMode: process.env.NODE_ENV !== 'development',
+  testMode: !process.env.DISABLE_TEST_MODE,
   loggerFn: ignoreLogger,
 })
