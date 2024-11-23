e316716e729092d417ee845274b74319b3d67024
huynamboz <teahuynam2003@gmail.com>
2024-11-23 22:11:12 +0700

    chore: remove phone log field

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -165,7 +165,6 @@ export default NuxtAuthHandler({
               details: {
                 provider: account.provider,
                 email: user.email,
-                phone: user.phone,
               },
             })
 
