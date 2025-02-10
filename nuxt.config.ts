4447245e7a9fd8b0316630a48485ffd09a573b27
huynamboz <teahuynam2003@gmail.com>
2025-02-10 16:36:17 +0700

    feat: migrate subscription management page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -148,6 +148,10 @@ export default defineNuxtConfig({
         tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE),
         replaysOnErrorSampleRate: Number(process.env.SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE),
       },
+
+      stripe: {
+        customerPortalURL: process.env.STRIPE_CUSTOMER_PORTAL,
+      },
     },
   },
 
