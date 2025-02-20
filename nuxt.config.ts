d88b0ad6d460a9014a1b9f80f76a7088b78f1977
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-02-21 01:06:14 +0700

    chore: minimize env credentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -87,7 +87,7 @@ export default defineNuxtConfig({
       cookieEncryptionKey: process.env.LOGTO_COOKIE_ENCRYPTION_KEY,
       fetchUserInfo: true,
       resources: [
-        process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
+        process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
       ],
       scopes: [
         'create:upload',
@@ -120,7 +120,7 @@ export default defineNuxtConfig({
       appCreditURL: process.env.NUXT_PUBLIC_APP_CREDIT_URL || 'http://thecodeorigin.com',
       appCreditEmail: process.env.NUXT_PUBLIC_APP_CREDIT_EMAIL || 'contact@thecodeorigin.com',
       appBaseUrl: process.env.NUXT_PUBLIC_APP_BASE_URL || 'http://localhost:3000',
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
       appPaymentRedirect: process.env.NUXT_PUBLIC_APP_PAYMENT_REDIRECT || '/app/settings/credit',
 
       features: {
@@ -284,10 +284,10 @@ export default defineNuxtConfig({
   vuefire: {
     config: {
       apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
       databaseURL: process.env.FIREBASE_DB_URL,
       projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.FIREBASE_APP_ID,
       measurementId: process.env.FIREBASE_MEASUREMENT_ID,
--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -9,10 +9,10 @@ const commandOptions = {
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
   databaseURL: process.env.FIREBASE_DB_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
