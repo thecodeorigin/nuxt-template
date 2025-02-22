85da4d081a7eb792efcc2f2e20485dd1c0d518ce
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-02-23 00:25:12 +0700

    feat: add title and desc to the credit package

--- server/db/schemas/credit_packages.schema.ts
+++ server/db/schemas/credit_packages.schema.ts
@@ -2,6 +2,8 @@ import { jsonb, numeric, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-c
 
 export const creditPackageTable = pgTable('credit_packages', {
   id: uuid('id').defaultRandom().primaryKey().notNull(),
+  title: text('title'),
+  description: text('description'),
   price: numeric('price').notNull(),
   currency: text('currency').notNull(),
   amount: numeric('amount').notNull(),
