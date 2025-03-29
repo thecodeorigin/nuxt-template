228452f9e7e6e663bc2e0ffb81118591dd13aa88
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-03-29 23:48:55 +0700

    chore: remove default og meta

--- app/app.vue
+++ app/app.vue
@@ -18,14 +18,7 @@ useHead({
   },
 })
 
-const title = 'Nuxt Template - SaaS template'
-const description = 'Nuxt Template is a collection of premium Vue components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes.'
-
 useSeoMeta({
-  title,
-  description,
-  ogTitle: title,
-  ogDescription: description,
   ogImage: '/social-card.png',
   twitterImage: '/social-card.png',
   twitterCard: 'summary_large_image',
