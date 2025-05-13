3c613f4f6ec93558a116b2829ca353354830dc83
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-05-14 00:11:51 +0700

    fix: exclude request logging for healthcheck api

--- server/plugins/logger.ts
+++ server/plugins/logger.ts
@@ -1,11 +1,14 @@
 import type { H3Event } from 'h3'
 
+const EXCLUDED_PATHS = ['/api/health']
+
 export default defineNitroPlugin(async (nitroApp) => {
   // Create a request logger function
   const logRequest = logger.createRequestLogger()
 
   // Hook into each request
   nitroApp.hooks.hook('request', async (event: H3Event) => {
-    await logRequest(event)
+    if (!EXCLUDED_PATHS.includes(event.path))
+      await logRequest(event)
   })
 })
