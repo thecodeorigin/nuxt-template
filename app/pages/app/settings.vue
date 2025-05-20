2357a94090c036762bb5b43d5425f7aca12601ef
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-05-20 23:10:57 +0700

    fix: setting max width

--- app/pages/app/settings.vue
+++ app/pages/app/settings.vue
@@ -5,7 +5,7 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full lg:max-w-2xl mx-auto">
+  <div class="flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full max-w-2xl mx-auto">
     <NuxtPage />
   </div>
 </template>
