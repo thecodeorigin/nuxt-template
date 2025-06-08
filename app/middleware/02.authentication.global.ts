f6568be6720cc042da36f345e164a2a73b76549b
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2025-06-09 00:43:09 +0700

    fix: auth redirect

--- app/middleware/02.authentication.global.ts
+++ app/middleware/02.authentication.global.ts
@@ -4,16 +4,16 @@ export default defineNuxtRouteMiddleware(async (to) => {
 
   const authStore = useAuthStore()
 
+  try {
+    await authStore.fetchProfile()
+  }
+  catch {}
+
   if (to.meta.unauthenticatedOnly && authStore.currentUser) {
     return navigateTo({ path: '/dashboard' })
   }
 
   if (to.meta.auth || to.meta.auth === undefined) {
-    try {
-      await authStore.fetchProfile()
-    }
-    catch {}
-
     if (!authStore.currentUser) {
       return navigateTo({ path: '/auth/login' })
     }
