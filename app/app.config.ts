9f21eadaf381d563d799d6bdb6d6884cb031aa34
Nguyen Huu Nguyen Y <nguyenhuunguyeny.ny@gmail.com>
2026-05-23 10:35:07 +0700

    feat: update theme and typescript source

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,20 +1,8 @@
 export default defineAppConfig({
   ui: {
-    borderRadius: 0,
     colors: {
       primary: 'blue',
       neutral: 'gray',
-    },
-    button: {
-      defaultVariants: {
-        size: 'md',
-        color: 'primary',
-      },
-    },
-    input: {
-      defaultVariants: {
-        size: 'md',
-      },
     },
   },
 })
